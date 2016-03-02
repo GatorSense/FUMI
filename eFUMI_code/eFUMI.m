57ee60e5c21b72b6463e2c930d4c057281b55bd1
Jordan Backes <jjbxtd@mail.missouri.edu>
2016-03-02 16:55:38 -0600

    changes to eFUMI and initializations given to me by Changzhe

--- eFUMI_code/eFUMI.m
+++ eFUMI_code/eFUMI.m
@@ -86,7 +86,13 @@ W(index_plus)=W_plus;
 W(index_minus)=W_minus;
 
 %initialize
-[E_initial,P]=eFUMI_VCA_initialize(X,labels,parameters);
+
+if parameters.init_flag==1
+    [X_pre, E_initial,P]=eFUMI_VCA_initialize(X,labels,parameters);
+elseif parameters.init_flag==2
+    [X_pre, E_initial,P]=eFUMI_KM_initialize(X,labels,parameters);
+end
+
 E=E_initial;
 obj_func=inf;
 Cond=inf;
@@ -94,21 +100,21 @@ Cond=inf;
 for i=1:parameters.iterationCap
     
 %E-step
-    Prob_Z=eFUMI_Prob_Z_Update(X,P,E,labels,parameters);%Estimate probability to be true positive
+    Prob_Z=eFUMI_Prob_Z_Update(X_pre,P,E,labels,parameters);%Estimate probability to be true positive
     
 %M-step
     %update P
     P_old=P;
     gamma_vecs=[0;(parameters.gammaconst./(sum(P_old(2:end,:),2)))];
-    P=eFUMI_P_Update(X,E,Prob_Z,labels,parameters,gamma_vecs);
+    P=eFUMI_P_Update(X_pre,E,Prob_Z,labels,parameters,gamma_vecs);
 
     %update E
     flag_E=parameters.flag_E;
-    E=eFUMI_E_Update(X,P,Prob_Z,labels,parameters,flag_E);
+    E=eFUMI_E_Update(X_pre,P,Prob_Z,labels,parameters,flag_E);
     
     %Condition Update        
     obj_func_old=obj_func;
-    [Cond, obj_func]=eFUMI_Cond_Update(X,P,E,Prob_Z,W,obj_func_old,parameters,gamma_vecs);
+    [Cond, obj_func]=eFUMI_Cond_Update(X_pre,P,E,Prob_Z,W,obj_func_old,parameters,gamma_vecs);
     
     %display condition after each iteration
     fprintf(['Iteration ' num2str(i) '\n']);
--- eFUMI_code/eFUMI_VCA_initialize.m
+++ eFUMI_code/eFUMI_VCA_initialize.m
@@ -1,4 +1,4 @@
-function [E,P]=eFUMI_VCA_initialize(X,labels,parameters)
+function [Data_pre,E,P]=eFUMI_VCA_initialize(X,labels,parameters)
 
 % This function initializes eFUMI using VCA by computing corresponding unmixing errors
 
@@ -15,6 +15,7 @@ function [E,P]=eFUMI_VCA_initialize(X,labels,parameters)
 %   labels - binary values, the same size as input data, indicates positive bags with logical '1'
 %
 % Outputs:
+%   Data_pre - preprocessed data after normalization denoted by parameters.norm_flag
 %   E - Initial Endmembers value, d and M account for wavelength bands and number of background endmembers,respectively
 %   P - Initial Proportion Values, M+1 by N, N accounts for the total number of input data
 %
@@ -67,6 +68,15 @@ X_plus=X(:,index_plus);
 X_minus=X(:,index_minus);
 N_plus=size(X_plus,2);
 N_minus=size(X_minus,2);
+
+% pre process data
+if parameters.norm_flag==1
+    Data_pre=normalize(X,1);
+elseif parameters.norm_flag==2
+    Data_pre=normalize(X,2);
+else
+    Data_pre=X;
+end
 
 %E initialization
 
